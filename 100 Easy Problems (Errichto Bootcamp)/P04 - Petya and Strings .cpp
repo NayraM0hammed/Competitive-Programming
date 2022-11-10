e6abe59c9ee203f4b543a182cadcbb86335fcb45
Nayra Mohammed <97265976+NayraM0hammed@users.noreply.github.com>
2022-11-10 18:29:23 +0200

    Create P04 - Petya and Strings .cpp

---
+++ 100 Easy Problems (Errichto Bootcamp)/P04 - Petya and Strings .cpp
@@ -0,0 +1,55 @@
+//"Perhaps..,we 're asking the wrong questions .." -Agent Brown
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <iomanip>
+#include <unordered_map>
+#define sz(s) (int(s.size()))
+#define MemS(s, x) memset(s, x, sizeof(s))
+#define PI acos(-1)
+typedef long long ll;
+using namespace std;
+const ll Mod = 1e9 + 7, INF = 1e18;
+const ll N = 1e6 + 1, O_O = LONG_LONG_MAX, T_T = INT_MAX, V_V = INT_MIN;
+//"Look deep into your soul, into the dark and foggy mist of your memories"
+
+void SADIEM()
+{
+ //   freopen("input.txt", "r", stdin);
+    //  freopen("output.txt", "w", stdout);
+
+    std::ios_base::sync_with_stdio(NULL);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+/*
+
+ “ I’d like to let you in on a very important secret I learned when I was about your age, boy.
+ You see, power, real power doesn’t come to those who were born strongest or fastest or smartest.
+ No. It comes to those who will do anything to achieve it.”
+
+*/
+void Catch_Kadr_fe_ALOLo()
+{
+    string a, b;
+    cin >> a >> b;
+    for (int i = 0; i < sz(a); i++)
+    {
+        a[i] = tolower(a[i]), b[i] = tolower(b[i]);
+        if (a[i] != b[i])
+            return void(cout << (a[i] > b[i] ? 1 : -1));
+    }
+    cout << 0;
+}
+int main()
+{
+    SADIEM();
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        Catch_Kadr_fe_ALOLo();
+        cout << (t ? "\n" : "");
+    }
+}
